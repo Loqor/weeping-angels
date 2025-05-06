30b154d13b2f37e6e522f9bf5439aac35d64f91b
Loqor <65447472+Loqor@users.noreply.github.com>
2025-05-06 04:00:10 -0500

    fix: gitignore feat: datagen

--- src/main/java/com/loqor/client/LoqorsWeepingAngelsDataGenerator.java
+++ src/main/java/com/loqor/client/LoqorsWeepingAngelsDataGenerator.java
@@ -1,11 +1,37 @@
 package com.loqor.client;
 
+import com.loqor.core.LWAEntities;
+import com.loqor.core.LWAItems;
+import dev.amble.lib.datagen.lang.AmbleLanguageProvider;
+import dev.amble.lib.datagen.lang.LanguageType;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
 
 public class LoqorsWeepingAngelsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+		generateENUSLanguage(pack);
 	}
+
+	public void generateENUSLanguage(FabricDataGenerator.Pack pack) {
+		pack.addProvider(
+				((output, registriesFuture) -> addEnglishTranslations(output, registriesFuture, LanguageType.EN_US)));
+	}
+
+	public AmbleLanguageProvider addEnglishTranslations(FabricDataOutput output,
+														CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture, LanguageType languageType) {
+		AmbleLanguageProvider provider = new AmbleLanguageProvider(output, languageType);
+
+		provider.addTranslation(LWAItems.ANGEL_SPAWNER_ITEM, "Angel Spawner");
+		provider.addTranslation(LWAEntities.WEEPING_ANGEL.getTranslationKey(), "Weeping Angel");
+
+		return provider;
+	}
+
+
 }
